0da6864367d348ebb92298f519e5ebe888b5a356
DennisDaddy <softteez95@gmail.com>
2017-09-06 03:36:27 +0300

    add methods

--- wedding.rb
+++ wedding.rb
@@ -1,2 +1,20 @@
-class Wedding
+def mrs_mentor
+	puts "hello guys lets talk serious business now!"
+	puts "when will huzzy and wife get married??"
+	
 end
+
+def huzzy(respond)
+	puts "#{respond}, Stop the shit i wish you a goodnight"
+	
+end
+
+
+def wife(respond)
+	puts "#{respond}, God's time is the best i think the next one year"
+	
+end
+
+mrs_mentor
+huzzy("Hello how are you")
+wife("Heey sweetheart are you for real?????")
